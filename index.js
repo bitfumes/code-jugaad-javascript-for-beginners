86b8399b089d488c08012bc552aa4252e4748b0e
sarthaksavvy <sarthak@bitfumes.com>
2024-01-20 08:40:05 +0530

    [main] 008 javascript comparison, bitwise Operators

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let a = "sarthak";
-let b = "3";
-let result = b - a;
-console.log(result);
+let a = 5;
+let b = 2;
+// console.log(5 & 2); // AND OPERATION
+// console.log(5 | 3); // OR OPERATION
+// console.log(~5); // NOT OPERATION
+// console.log(5 >> 3); // Shift OPERATION
+// 5 = 000000000000
+// ~5 = 1010
