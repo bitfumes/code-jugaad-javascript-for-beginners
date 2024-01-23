0c78a9ece680913df09e5fec9f346b3d645c3027
sarthaksavvy <sarthak@bitfumes.com>
2024-01-23 09:54:48 +0530

    [main] 016 javascript why need functions

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-let car = "Tesla";
-let myCar = car;
-myCar = "BMW";
-console.log(car, myCar);
+let result;
 
-let user = {
-  name: "John",
-  age: 30,
-  meta: {
-    country: "India",
-    city: "Delhi",
-    address: {
-      street: "123 Main St",
-      zipcode: "12345",
-    },
-  },
+let sum = function (a, b) {
+  if (a === undefined || b === undefined) return;
+
+  result = a + b;
+  return result;
 };
 
-let admin = structuredClone(user);
-user.meta.city = "Mumbai";
-user.meta.address.zipcode = "00000";
-
-console.log(admin.meta, user.meta);
+console.log(sum());
