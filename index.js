aaaa825d00adc5755c33f9ebde53684c30f254ae
sarthaksavvy <sarthak@bitfumes.com>
2024-01-24 07:33:39 +0530

    [main] 017 javascript arrow function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const add = (a, b) => a + b;
+
 let result;
 
-let sum = function (a, b) {
+let sum = (a, b) => {
   if (a === undefined || b === undefined) return;
 
   result = a + b;
   return result;
 };
 
-console.log(sum());
+console.log(add(34, 6565));
