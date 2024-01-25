deeeed89aaad5451b4bc1d60e5945a0b94b7fc02
sarthaksavvy <sarthak@bitfumes.com>
2024-01-25 07:29:10 +0530

    [main] 020 javascript filter some every

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-let fruit = "apple";
+const scoreCard = [
+  { name: "Math", score: 80 },
+  { name: "Science", score: 60 },
+  { name: "History", score: 74 },
+  { name: "Geography", score: 60 },
+  { name: "English", score: 75 },
+];
 
-const user = {
-  name: "John",
-  age: 30,
-  test: () => {
-    console.log("yahi pe code hai");
-    console.log(this);
-  },
-};
+const punishmentMilega = scoreCard.some((subject) => {
+  console.log(subject.name);
+  return subject.score < 70;
+});
 
-const admin = {
-  name: "Admin",
-};
-
-const myTest = user.test.bind(admin);
-myTest();
+console.log(punishmentMilega);
