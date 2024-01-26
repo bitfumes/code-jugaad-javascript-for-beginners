7bd0032a1bd2a4a33929189142fe7241401d097c
sarthaksavvy <sarthak@bitfumes.com>
2024-01-26 07:58:34 +0530

    [main] 023 javascript string

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const mathTest = {
-  name: "Mathtest",
-  score: 80,
-  chapter: [1, 2, 3],
-  students: 10,
-};
-
-Object.defineProperty(mathTest, "teacher", {
-  value: "Sarthak",
-  enumerable: true,
-});
-
-console.log(mathTest);
+const myname = "    Sarthak";
+console.log(myname);
