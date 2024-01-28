e78aef4e0653a95840c31f1c81a08d034a6901bd
sarthaksavvy <sarthak@bitfumes.com>
2024-01-28 08:52:00 +0530

    [main] 025 javascript class

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const email = "sarthak@bitfumes.com";
-const email2 = "john@gmail.com";
-console.log(email2.startsWith("sarthak"));
+class Rectagle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+}
+
+const rect = new Rectagle(3, 41);
+console.log(rect.area());
