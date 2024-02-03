31f2ac011f4f8a3e25740ade7dce886213af557e
sarthaksavvy <sarthak@bitfumes.com>
2024-02-03 12:40:42 +0530

    [main] 029 javascript callback hell

--- index.js
+++ index.js
@@ -1,71 +1,20 @@
-class SuperArray {
-  isEmpty() {
-    return this.length === 0;
-  }
+// api call, user id
+// api call , product by user id
+// api call of product discount by product id
+
+function makeSandwich(callback) {
+  console.log("Step 1: Get some bread");
+  setTimeout(() => {
+    callback();
+    setTimeout(() => {
+      console.log("Step 3: Add some cheese");
+      setTimeout(() => {
+        console.log("Step 4: Add some jelly");
+      }, 1000);
+    }, 1000);
+  }, 1000);
 }
 
-const arr = new SuperArray(1, 2, 3, 4, 5);
-console.log(arr.isEmpty());
-console.log(arr.map((x) => x * 2));
-
-// class Shape {
-//   constructor(name) {
-//     this.name = name;
-//   }
-
-//   area(area) {
-//     if (!area) {
-//       return "Area function is not implemented in this class";
-//     }
-
-//     console.log("Area is : ", area);
-//   }
-
-//   print(msg) {
-//     console.log(msg);
-//   }
-// }
-
-// class Rectagle extends Shape {
-//   constructor(width, height) {
-//     super("Rectagle");
-//     this.width = width;
-//     this.height = height;
-//   }
-
-//   area() {
-//     super.area(this.width * this.height);
-//     return this.width * this.height;
-//   }
-// }
-
-// class Square extends Shape {
-//   constructor(side) {
-//     super("Square");
-//     this.side = side;
-//   }
-
-//   area() {
-//     return this.side * this.side;
-//   }
-// }
-
-// class Circle extends Shape {
-//   constructor(radius) {
-//     super("Circle");
-//     this.radius = radius;
-//   }
-
-//   area() {
-//     return Math.PI * this.radius * this.radius;
-//   }
-// }
-
-// const rect = new Rectagle(3, 41);
-// const sqr = new Square(3);
-// const cirlc = new Circle(7);
-// console.log(rect.area());
-// console.log(sqr.area());
-// console.log(cirlc.area());
-
-// cirlc.print("Hello World");
+makeSandwich(function () {
+  console.log("Step 2: Toast the bread");
+});
