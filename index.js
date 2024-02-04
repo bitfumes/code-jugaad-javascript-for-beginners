95a528430956f1a78a018779fa1687595ff1945b
sarthaksavvy <sarthak@bitfumes.com>
2024-02-04 21:18:25 +0530

    [main] 032 javascript module import export

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-async function step1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Step 1: Get some bread"), 1000);
-  });
-}
+import * as math from "./math.js";
 
-async function step2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Step 2: Toast the bread"), 1000);
-  });
-}
+console.log(math.sum(1, 2));
+console.log(math.sub(1, 2));
+console.log(math.mult(1, 2));
+console.log(math.div(1, 2));
 
-async function step3() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("Step 3: Add some cheese"), 1000);
-  });
-}
+import print from "./print.js";
 
-async function step4() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("Step 4: Add some jelly"), 1000);
-  });
-}
-
-async function main() {
-  try {
-    const res1 = await step1();
-    console.log(res1);
-    const res2 = await step2();
-    console.log(res2);
-    const res3 = await step3();
-    console.log(res3);
-    const res4 = await step4();
-    console.log(res4);
-  } catch (e) {
-    console.log("Error: ", e);
-  }
-}
-main();
+print.printName("Sarthak");
+print.printError("Error");
---
+++ print.js
@@ -0,0 +1,9 @@
+function printName(msg) {
+  console.log(msg);
+}
+
+function printError(err) {
+  console.error(err);
+}
+
+export default { printName, printError };
