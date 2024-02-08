eb2356edc63dddddecba9817e32b4bb4e3ab654d
sarthaksavvy <sarthak@bitfumes.com>
2024-02-08 15:44:20 +0530

    [main] 034 javascript reflect and proxy deleteProperty

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-
-
 let user = {
-    _password : "Secret"
-}
+  _password: "Secret",
+  get myname() {
+    return this.name;
+  },
+};
 
 const option = {
-    get(target, prop) {
-        if(prop.startsWith("_"))
-            throw new Error("Access Denied")
-        return target[prop]
-    },
-    ownKeys(target){
-        return Object.keys(target).filter(key => !key.startsWith("_"))
-    },
-    set(target, prop, val){
-        if(prop === "salary")
-            return false
-        target[prop] = val
-        return true
-    }
-}
+  get(target, prop, receiver) {
+    if (prop.startsWith("_")) throw new Error("Access Denied");
+    return Reflect.get(...arguments);
+  },
+  ownKeys(target) {
+    return Object.keys(target).filter((key) => !key.startsWith("_"));
+  },
+  set(target, prop, val) {
+    if (prop.startsWith("_")) throw new Error("Access Denied");
+    if (prop === "salary") return false;
+    Reflect.set(...arguments);
+    return true;
+  },
+  deleteProperty(target, prop) {
+    if (prop.startsWith("_")) throw new Error("Access Denied");
+    Reflect.deleteProperty(...arguments);
+    return true;
+  },
+};
 
 user = new Proxy(user, option);
-user.name = "Sarthak"
-user.age = 23
-console.log(Object.keys(user));
-console.log(user._password);
+user.name = "Sarthak";
+user.age = 23;
+
+const admin = {
+  __proto__: user,
+  name: "Admin",
+};
+console.log(user.name);
+Reflect.set(admin, "name", "John");
+console.log(admin.myname);
