6b78915937ab4a96b87b0d6c250cf6ba7eb406ad
sarthaksavvy <sarthak@bitfumes.com>
2024-02-08 13:32:49 +0530

    [main] 033 javascript proxy

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-import * as math from "./math.js";
 
-console.log(math.sum(1, 2));
-console.log(math.sub(1, 2));
-console.log(math.mult(1, 2));
-console.log(math.div(1, 2));
 
-import print from "./print.js";
+let user = {
+    _password : "Secret"
+}
 
-print.printName("Sarthak");
-print.printError("Error");
+const option = {
+    get(target, prop) {
+        if(prop.startsWith("_"))
+            throw new Error("Access Denied")
+        return target[prop]
+    },
+    ownKeys(target){
+        return Object.keys(target).filter(key => !key.startsWith("_"))
+    },
+    set(target, prop, val){
+        if(prop === "salary")
+            return false
+        target[prop] = val
+        return true
+    }
+}
+
+user = new Proxy(user, option);
+user.name = "Sarthak"
+user.age = 23
+console.log(Object.keys(user));
+console.log(user._password);
