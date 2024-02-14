8eb1d0a0d69ee6b32c2cfaa6fcfe88ed7ce45135
sarthaksavvy <sarthak@bitfumes.com>
2024-02-14 09:10:22 +0530

    [main] 005 practice javascript variables

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
-function runCode() {
-  const codeText = document.getElementById("code").value;
+/**
+ * Question 1
+ * Explain the difference between global, local, and block scope .
+ */
+// myname = "Sarthak";
 
-  // eval(codeText);
-  const runMyCode = new Function("", codeText);
-  runMyCode();
-}
+// function printName() {
+//   var favFood = "pizza";
+//   console.log(favFood);
+// }
+
+// console.log(favFood);
+
+// if (true) {
+//   let favSport = "Cricket";
+//   console.log(favSport);
+//   console.log("inside if ", myname);
+// }
+
+// console.log(favSport);
+
+/**
+ * Question 2
+ * Explain the concept of hoisting in JavaScript.
+ */
+
+// test();
+
+// function test() {
+//   console.log("Hello");
+// }
+
+// console.log(a);
+// var a = 3;
+
+// console.log(b);
+// let b = 4;
+
+/**
+ * Question 3
+ * Reassigning Values with const.
+ */
+
+// const PI = 3.14;
+// console.log(PI);
+// PI = 5.454;
+// console.log(PI);
+
+// const user = {
+//   name: "Sarthak",
+// };
+// user.name = "John";
+// console.log(user);
+
+/**
+ * Question 4
+ * Explain the relationship between global variables and the window object.
+ */
+// var mypet = "Dog";
+// console.log(window.mypet);
+
+/**
+ * Question 5
+ * Describe the behavior of variables declared with var, let, and const inside a loop.
+ */
+
+// for (const i = 0; i < 5; i++) {
+//   console.log(i);
+//   // setTimeout(() => {
+//   // }, 100);
+// }
