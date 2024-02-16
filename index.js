4f9f4d1444f169a36a0a7e505022e88310f40849
sarthaksavvy <sarthak@bitfumes.com>
2024-02-16 12:33:47 +0530

    [main] 011 javascript arrays spread flat, includes and index

--- index.js
+++ index.js
@@ -1,71 +1,50 @@
 /**
  * Question 1
- * Explain the concept of the ternary operator
+ * Explain the difference between pop and shift
  */
 
-// let x = 0;
-// const result = x === 0 ? 100 : 1000;
-
-// if (x == 0) {
-//   console.log("value is zero");
-// } else {
-//   console.log("value is non zero");
-// }
+// const arr = [10, 1, 2, 3, 4, 5, 4, 3, 2];
+// console.log(arr.unshift());
+// console.log(arr);
+// const removedElement = arr.pop();
+// console.log(removedElement);
+// console.log(arr);
 
 /**
  * Question 2
- * How would you use the nullish coalescing operator (??)
+ * What happens if you call shift on an empty array?
  */
 
-// let username = "Sarthak";
-
-// const nameToShow = username ?? "Guest"
+// const arr = [];
+// console.log(arr.shift());
+// console.log(arr);
 
 /**
  * Question 3
- * Explain the difference between "if-else" and "switch" statements
+ *  insert an element at a specific index in an array without using splice?
  */
-// const trafficLight = "red";
-// switch (trafficLight) {
-//   case "red":
-//     console.log("stop");
-//     break;
-//   case "yellow":
-//     console.log("get ready ! alert");
-//     break;
-//   case "green":
-//     console.log("go ahead");
-//     break;
-//   default:
-//     console.log("Marzi ke maalik");
-//     break;
-// }
-// if (trafficLight == "red") {
-//   console.log("stop");
-// } else if (trafficLight == "yellow") {
-//   console.log("get ready ! alert");
-// } else if (trafficLight == "green") {
-//   console.log("go ahead");
-// } else {
-//   console.log("Marzi ke maalik");
-// }
+
+// const arr = ["a", "b", "c"];
+// arr.splice(2, 0, "d");
+// console.log(arr);
+// const part1 = arr.slice(0, 2);
+// const part2 = arr.slice(2);
+// const finalArr = part1.concat("d", part2);
+// console.log(finalArr);
 
 /**
  * Question 4
- * Explain the concept of truthy and falsy values in JavaScript
+ * Write a function that removes the first element from an array without using the shift method.
  */
-// let name = "";
-// // falsy : 0, NaN, null, undefined, false, ""
-// if (name) {
-//   console.log("Truthy");
-// } else {
-//   console.log("Falsy");
-// }
 
+// const arr = ["apple", "mango", "graps"];
+// const finalArr = arr.splice(1);
+// const finalArr = arr.slice(1);
+// console.log(finalArr);
 /**
  * Question 5
- * Explain the concept of the "falsy bouncer" pattern in JavaScript
+ * How can you efficiently clear all elements from a JavaScript array without reassigning it
  */
-
-const arr = [0, 1, 2, 3, 4];
-console.log(arr.filter((item) => item));
+// let arr = ["apple", "mango", "graps"];
+// arr.length = 0;
+// console.log(arr);
