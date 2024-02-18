db18190b221092f1d3f775a60d128b57998043b7
sarthaksavvy <sarthak@bitfumes.com>
2024-02-18 19:17:48 +0530

    [main] 013 Practice JavaScript objects copy

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
 /**
  * Question 1
- * How can you determine if a property exists in a JavaScript object
+ * Explain the concept of property descriptors in JavaScript objects.
  */
-// const user = {
-//   name: "Sarthak",
-//   profession: "Software Developer",
-// };
-// if ("name" in user) {
-//   console.log(user.name);
-// }
+// const user = { name: "sarthak" };
+// console.log(Object.getOwnPropertyDescriptors(user));
+
 /**
  * Question 2
- * Explain the difference between dot notation and bracket notation when accessing object properties.
+ * What is the difference in Object.freeze(), Object.preventExtensions & Object.seal() method?
  */
 
-// const mycar = {
-//   company: "Tesla",
-//   "0 100": "4.2second",
-// };
-// console.log(mycar["company"]);
+// const user = { name: "sarthak" };
+// Object.preventExtensions(user); // add not allow
+// Object.seal(user); // delete and add not allow
+// Object.freeze(user); // update, delete and add not allow
+// console.log(Object.getOwnPropertyDescriptors(user));
+// delete user.name;
+// console.log(user);
 
 /**
  * Question 3
- * How do you iterate over the properties of an object?
+ * What is the difference between Object.values() and Object.entries()?
  */
 
-// const fruit = {
-//   name: "Apple",
-//   amount: "2kg",
-// };
-// const keys = Object.keys(fruit);
-// for (let i = 0; i < keys.length; i++) {
-//   console.log(fruit[keys[i]]);
-// }
-// for (prop in fruit) {
-//   console.log(fruit[prop]);
-// }
+// const user = { name: "sarthak", isAdmin: true };
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
 
 /**
  * Question 4
- * Explain the difference between deep and shallow copying of objects.
+ * What are JavaScript Object Prototypes ?
  */
-
-// const obj = { name: "John", address: { city: "New York" } };
-// const shallowCopy = { ...obj };
-// shallowCopy.address.city = "Delhi";
-// console.log(shallowCopy.address.city, obj.address.city);
-// const deepCopy = JSON.parse(JSON.stringify(obj));
-// deepCopy.address.city = "Delhi";
-// console.log(deepCopy.address.city, obj.address.city);
-
+// const user = { name: "sarthak" };
+// const new_user = Object.create(user);
+// new_user.name = "Billi";
+// console.log(new_user.name);
 /**
  * Question 5
- * What is the purpose of the Object.is() method in JavaScript?
+ * How to create private proterties in Javascript object
  */
 
-// console.log(Object.is(undefined, null));
+// const createUser = () => {
+//   let name = "Sarthak";
+
+//   return {
+//     // getName() {
+//     //   return name;
+//     // },
+//   };
+// };
+
+// const user = createUser();
+
+// console.log(user.getName());
