e1c444a442674eff09753779f4b051f04143d56e
sarthaksavvy <sarthak@bitfumes.com>
2024-03-03 13:53:42 +0530

    [main] 021 Practice JavaScript reduce function

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
 /**
  * Question 1
- * Write a function that filters out all negative numbers from an array using the filter method.
+ * How can you use the Array.reduce function to flatten an array of arrays?
+ *
  */
 
-// const myarr = [-10, 160, 87, 45, -120, 92, -73];
-// const newMyArr = myarr.filter((num) => num >= 0);
-// console.log(newMyArr);
+const nestedArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const newFlatArr = nestedArray.reduce((acc, curr) => {
+  return acc.concat(curr);
+}, []);
+console.log(newFlatArr);
 /**
  * Question 2
- * Write a function using some to check if there is at least one element in an array that is a palindrome (reads the same backward as forward).
+ * How can you find the maximum element in an array using Array.reduce?
+ *
  */
 
-const stringArray = ["level", "hello", "racecar", "world"];
-
-function hasPalindrom(word) {
-  return word === word.split("").reverse().join("");
-}
-const arrayIsPalindrom = stringArray.some((word) => hasPalindrom(word));
-console.log(arrayIsPalindrom);
+const numbers = [10, 20, 30, 40, 50];
+const max = numbers.reduce((acc, curr) => {
+  return acc > curr ? acc : curr;
+}, 0);
+console.log(max);
 
 /**
  * Question 3
- * Write a function that filter out on any criteria that user can provide in a callback function
+ * How can you find the average of an array using Array.reduce?
+ *
  */
 
-// const filter = (arr, callback) => {
-//   const filteredArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     callback(arr[i]) ? filteredArr.push(arr[i]) : "";
-//   }
-//   return filteredArr;
-// };
+const numbers1 = [10, 20, 30, 40, 50];
+const avg = numbers1.reduce((acc, curr, index) => {
+  if (index === numbers1.length - 1) {
+    return (acc + curr) / numbers1.length;
+  } else {
+    return acc + curr;
+  }
+}, 0);
 
-// const myarr = [-10, 160, 87, 45, -120, 92, -73];
-// const newMyArr = filter(myarr, (num) => num % 2 == 0);
-// console.log(newMyArr);
-
-/**
- * Question 4
- * Write a function customSome that replicates the functionality of the some method
- */
-
-// const customSome = (arr, callback) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (callback(arr[i])) {
-//       return true;
-//     }
-//   }
-
-//   return false;
-// };
-
-// const arrOfNumbers = [1, 3, 5, 7, 9];
-// const isEven = customSome(arrOfNumbers, (num) => num % 2 === 0);
-// console.log(isEven);
-
-/**
- * Question 5
- * Write a function customEvery that replicates the functionality of the every method
- */
+console.log(avg);
