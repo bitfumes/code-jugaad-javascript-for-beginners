90f94dc60fb72b73b518ef4d51368c8a712292c6
sarthaksavvy <sarthak@bitfumes.com>
2024-03-08 09:58:50 +0530

    [main] 025 Practice JavaScript class

--- index.js
+++ index.js
@@ -1,25 +1,72 @@
 /**
  * Question 1
- * Remove all whitespace characters from a given string.
+ * Explain the difference between a class declaration and a class expression.
  */
 
-// const str1 = "  hello   world  ";
-// console.log(str1.replace(/\s\s/g, ""));
+// class Animal2 {} // class declaration
+
+// const Animal1 = class {}; // class expression
 
 /**
  * Question 2
- * Convert a string to title case (first letter of each word capitalized).
+ * Explain the purpose of the constructor method in a JavaScript class.
  *
  */
 
-// const str2 = "hello world";
-// console.log(str2.replace(/\b\w/g, (char) => char.toUpperCase()));
+// class Animal {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const dog = new Animal("Tiger", 5);
 
 /**
  * Question 3
- * Truncate a string and add ellipsis if it exceeds a certain length.
+ * How can you achieve private members in a JavaScript class?
  *
  */
 
-// const str3 = "Lorem ipsum dolor sit amet";
-// console.log(str3.length > 10 ? str3.substring(0, 10) + "..." : str3);
+// const Counter = class {
+//   #count = 0;
+
+//   increment() {
+//     this.#count++;
+//   }
+
+//   decrement() {
+//     this.#count--;
+//   }
+
+//   getCount() {
+//     return this.#count;
+//   }
+// };
+
+// const mycounter = new Counter();
+// mycounter.increment();
+// mycounter.increment();
+// console.log(mycounter.getCount()); // 0
+// mycounter.decrement();
+// console.log(mycounter.getCount()); // 0
+
+/**
+ * Question 4
+ * What is the static keyword in a class and how is it used?
+ *
+ */
+
+// class MyMath {
+//   constructor() {
+//     this.count = 10;
+//     console.log("constructor");
+//   }
+
+//   static square(x) {
+//     return x * x + this.count;
+//   }
+// }
+
+// const squareOf4 = MyMath.square(4);
+// console.log(squareOf4);
